d92853a9b9a23f2cbccd1aa808c952eb57a1e727
min-seo7 <mms1117777@gmail.com>
2025-08-08 17:11:47 +0900

    min/발주등록-행추가/250808 17:11

---
+++ client/src/components/stock/stockCommTable.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: 'stockCommTable',
+    props: {
+        columns: {
+            type: Array,
+            required: true
+            // 예: [{ field: 'reDate', header: '등록일' }, ...]
+        },
+        rows: {
+            type: Array,
+            required: true
+        }
+    }
+};
+</script>
+
+<template>
+    <div class="card w-full">
+        <DataTable :value="rows" dataKey="id" tableStyle="min-width: 50rem">
+            <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
+
+            <!-- 동적 컬럼 렌더링 -->
+            <Column v-for="col in columns" :key="col.field" :field="col.field" :header="col.header" />
+        </DataTable>
+    </div>
+</template>
