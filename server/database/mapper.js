197063950dceed40341b68cb3b51160698dea02f
admin <pc@pc.com>
2025-08-10 22:33:04 +0900

    김태희/출하등록,반품등록,수정등록화면구성/2025-08-10

--- server/database/mapper.js
+++ server/database/mapper.js
@@ -2,16 +2,19 @@
 // 주의 마리아db는 req 부분이 2가지
 const mariadb = require("mariadb");
 const sqlList = require("./sqlList.js");
-require("dotenv").config({ path: "./sqls/.env" }); //env 파일 경로를 path지정
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "sqls/.env") });
+
 //몇개의 커넥션을 만들어두고 진행
 const connectionPool = mariadb.createPool({
   //DB에 접속하는 정보,네이버 공인 아이디 변경
-  host: process.env.DB_host,
-  port: process.env.DB_port,
-  user: process.env.DB_user,
-  password: process.env.DB_password,
-  database: process.env.DB_database,
-  connectionLimit: process.env.DB_Limit,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: process.env.DB_LIMIT,
+  connectTimeout: 5000,
   //추가옵션
   insertIdAsNumber: true, //자동으로 넘버타입으로 넘어오지x
   bigIntAsNumber: true, //카운터함수로 실행하면 bigInt데이터타입이라 자바스크립트의 자동매핑되는 타입x 넘버로 자동 변환
@@ -21,6 +24,12 @@ const connectionPool = mariadb.createPool({
     error: console.log,
   },
 });
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_LIMIT:", process.env.DB_LIMIT);
 //단건실행이라 오토커밋이라 롤백이랑 커밋등이 필요x 콜백x 프로미스 방식
 const query = async (alias, values) => {
   let conn = null;
