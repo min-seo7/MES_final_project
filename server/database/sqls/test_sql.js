b3ac4ced7de5b8083b00d7fad3f0e3daa6029a65
Sangwoo, Kim <kimsw5861@naver.com>
2025-08-13 17:17:41 +0900

    20250813

--- server/database/sqls/test_sql.js
+++ server/database/sqls/test_sql.js
@@ -16,21 +16,40 @@ const selectItemdetail = `
 SELECT *
 FROM testitem
 WHERE (? IS NULL OR ? = '' OR product_type LIKE CONCAT('%', ?, '%'))
-  AND (? IS NULL OR ? = '' OR purpose_name LIKE CONCAT('%', ?, '%'));`;
+  AND (? IS NULL OR ? = '' OR purpose_name LIKE CONCAT('%', ?, '%'))
+ORDER BY testitem_code DESC`;
 
 // 검사항목등록
 const insertItem = `
-INSERT INTO testitem(testitem_code, 
-                     product_type, 
-                     item_name, 
-                     unit, 
+INSERT INTO testitem(product_type, 
+                     item_name,
+                     unit,
+                     fixedStandard,
                      createdBy,
-                     purpose_name)
-VALUES(?,?,?,?,?,?)
+                     purpose_name,
+                     purpose_id)
+VALUES(?,?,?,?,?,?,?)
 `;
+
+// 자재 입고검사 대기목록
+const selectInspecWaitList = `
+SELECT
+    PD.purch_id AS 발주번호,      
+    PD.material_id AS 자재코드,  
+    MI.qty AS 수량_EA,         
+    MI.inspStatus AS 상태            
+FROM
+    tbl_purchase_detail AS PD
+INNER JOIN
+    material_Inspection AS MI
+ON
+    PD.purch_id = MI.purch_id
+WHERE 
+	MI.inspStatus = '대기'`;
 
 module.exports = {
   selectItem,
   insertItem,
   selectItemdetail,
+  selectInspecWaitList,
 };
