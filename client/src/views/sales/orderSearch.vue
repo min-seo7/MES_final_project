fcc54241cd8713fdb20ee95a44d1103e96a418eb
your name1 <you@example.com>
2025-08-22 19:32:33 +0900

    sorry

---
+++ client/src/views/sales/orderSearch.vue
@@ -0,0 +1,12 @@
+<script setup>
+import OrderRegisList from '@/components/sales/orderRegist/orderRegisList.vue';
+import { onUnmounted } from 'vue';
+onUnmounted(() => {
+    console.log('order.vue');
+});
+</script>
+<template>
+    <section>
+        <OrderRegisList />
+    </section>
+</template>
